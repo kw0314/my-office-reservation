f20798e172e0adcf2f715d3259b640ac04ae4c2e
kw0314 <kw0314@gmail.com>
2026-04-25 22:15:20 -0500

    관리자 전용 UI 개선

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,4 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import Http404, HttpResponseRedirect
+from django.urls import path, reverse
+from django.utils.html import format_html
+from django.core.exceptions import PermissionDenied
+
 from .models import Room, Reservation, Block, AccessDevice, AuditLog
 
 
@@ -11,11 +16,50 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ("room", "start_at", "end_at", "title", "status")
+    list_display = ("room", "start_at", "end_at", "title", "status", "approve_button")
     list_filter = ("status", "room")
     search_fields = ("title", "note_internal")
     ordering = ("-start_at",)
     actions = ["approve_reservations", "reject_reservations"]
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "<path:object_id>/approve/",
+                self.admin_site.admin_view(self.approve_view),
+                name="reservations_reservation_approve",
+            ),
+        ]
+        return custom_urls + urls
+
+    def approve_button(self, obj):
+        if obj.status == Reservation.STATUS_PENDING:
+            url = reverse("admin:reservations_reservation_approve", args=[obj.pk])
+            return format_html(
+                '<a class="button" href="{}">승인</a>',
+                url,
+            )
+        return "-"
+    approve_button.short_description = "승인"
+    approve_button.allow_tags = True
+
+    def approve_view(self, request, object_id):
+        reservation = self.get_object(request, object_id)
+        if reservation is None:
+            raise Http404("Reservation not found.")
+        if not self.has_change_permission(request, obj=reservation):
+            raise PermissionDenied
+
+        if reservation.status == Reservation.STATUS_PENDING:
+            reservation.status = Reservation.STATUS_CONFIRMED
+            reservation.save(update_fields=["status", "updated_at"])
+            self.message_user(request, f"예약 {reservation.id}을(를) 승인했습니다.")
+        else:
+            self.message_user(request, "이 예약은 이미 승인되었거나 취소된 상태입니다.", level=messages.WARNING)
+
+        redirect_url = request.GET.get("next") or reverse("admin:reservations_reservation_changelist")
+        return HttpResponseRedirect(redirect_url)
 
     @admin.action(description="선택한 예약을 승인(Confirmed) 처리합니다")
     def approve_reservations(self, request, queryset):
