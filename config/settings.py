e0c3215a2ef985501fea3c544cd4e860549a89ef
kw0314 <kw0314@gmail.com>
2026-06-13 22:50:41 -0500

    update

--- config/settings.py
+++ config/settings.py
@@ -135,3 +135,8 @@ USE_I18N = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/srv/catechism/staticfiles'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
